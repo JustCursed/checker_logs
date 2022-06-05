20efcbaf9e55ff3bf1ca1032c2f9bd50c67da7b4
JustFrog <mytojimbyylabu@gmail.com>
2022-06-05 22:31:37 +0300

    update

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,39 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, App, HttpServer, Responder};
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use tokio::io;
 
-#[get("/{id}/{name}/hi")]
-async fn index(params: web::Path<(u32, String)>) -> impl Responder {
-    let (id, name) = params.into_inner();
-    format!("Hello {}! id:{}", name, id)
+#[get("/{project}/{server}/index.html")]
+async fn index() -> impl Responder {
+	let path = Path::new("./logs/");
+	return cat(path);
 }
 
-#[actix_web::main] // or #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+	HttpServer::new(|| App::new().service(index))
+		.bind(("127.0.0.1", 8080))?
+		.run()
+		.await
 }
+
+fn cat(path: &Path) -> io::Result<String> {
+    let mut f = File::open(path)?;
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+//
+// fn main() {
+// 	let path = "paaaaa";
+// 	println!("{:?} ", cat(".gitignore".as_ref()));
+// }
+//
+// fn cat(path: &Path) -> io::Result<File> {
+//     let mut f = File::open(path);
+//     let mut s = String::new();
+//     return f.into_boxed_str();
+// }
